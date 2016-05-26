545a914a90803a01d8f50529807dd64e1afc7b12
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-05-26 11:02:55 -0400

    Added Utils.php file to be called upon regularly. Meaning that any function stored in the file will be used multiple times.

--- emalda-inc/WebContent/includes/Utils.php
+++ emalda-inc/WebContent/includes/Utils.php
@@ -1,19 +1,12 @@
 <?php
 
-	function check_input($data)
-	{
-		global $ret_data;
-		$data = trim($data);
-		$ret_data = htmlspecialchars($data);
-		return $ret_data;
-	}
-	
 	// sanitization of data
 	function check_input($data){
+		global $ret_data;
 		$data = trim($data);
 		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
+		$ret_data = htmlspecialchars($data);
+		return $ret_data;
 	}// End form validation/sanitization
 
 ?>
