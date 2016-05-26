f23b60d1f18d4f1cdc5b65cc2449ee105e86c177
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-05-26 12:16:40 -0400

    Created and updated form objects

--- emalda-inc/WebContent/includes/FormObjects.php
+++ emalda-inc/WebContent/includes/FormObjects.php
@@ -1,3 +1,30 @@
 <?php
 
+	// Class to construct Email with getters/setter
+	class EmailClass
+	{
+		// property declaration
+		private $email="";
+		 
+		// Constructor
+		public function __construct($email)
+		{
+			$this->email = $email;
+		}
+	
+		// Get methods
+		public function getEmail ()
+		{
+			return $this->email;
+		}
+		 
+		// Set method
+		public function setEmail ($value)
+		{
+			$this->email = $value;
+		}
+	
+	} // End Emailclass
+	
+
 ?>
