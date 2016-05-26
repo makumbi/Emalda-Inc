3d11d7d2efb666b6599f1ddcd190dcc4d06132c2
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-05-26 12:17:37 -0400

    Included findEmail sql function.

--- emalda-inc/WebContent/sql/SQLFunctions.php
+++ emalda-inc/WebContent/sql/SQLFunctions.php
@@ -5,5 +5,33 @@
 	
 	// Include the CustomerClass definition
 	require_once('includes/FormObjects.php');
+	
+	function findEmail ($email)
+	{
+		// Connect to the database
+		$mysqli = connectdb();
+		$customer_email = $email->getEmail();
+		 
+		// Connect to the database
+		$mysqli = connectdb();
+	
+		// Define the Query
+		// For Windows MYSQL String is case insensitive
+		$Myquery = "SELECT count(*) as count from Deals_Email
+		   where email=?";
+	
+		$result = $mysqli -> prepare($Myquery);
+		$result -> bind_param("s", $customer_email);
+	
+		if($result -> execute()){
+	
+			while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+				$count=$row["count"];
+		 }
+		 $result->close();
+		}
+		$mysqli->close();
+		return $count;
+	}
 
 ?>
