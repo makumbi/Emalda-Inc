3248748691d763f83863aa6740d2a8e21c66b6fc
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-05-26 10:58:55 -0400

    Added new SQLFunctions file. This file has updated contents.

---
+++ emalda-inc/WebContent/sql/SQLFunctions.php
@@ -0,0 +1,9 @@
+<?php
+
+	// Include the required DBConnection information
+	require_once('includes/Dbconnect.php');
+	
+	// Include the CustomerClass definition
+	require_once('includes/FormObjects.php');
+
+?>
