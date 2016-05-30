e3269e5be62028da21fe2bc7df53b07f267bc39d
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-05-29 23:06:39 -0400

    Created insert_product.php

---
+++ emalda-inc/WebContent/php1/insert_product.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Product</title>
+</head>
+<body bgcolor="skyblue">
+
+	<form action="insert_product.php" method="post" enctype="multipart/form-data">
+		<table align="center" width="1000" border="2" bgcolor="orange">
+		
+			<tr align="center">
+				<td colspan="8"><h2>Insert New Post Here</h2></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+			
+			<tr>
+				<td>Product Title:</td>
+				<td><input type="text" name="product_title"></td>
+			</tr>
+		</table>
+	</form>
+
+</body>
+</html>
