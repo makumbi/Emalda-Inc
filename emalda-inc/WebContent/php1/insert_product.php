c9ffd0a61d8120a97c474f845acac78a5ba20e03
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-05-29 23:20:14 -0400

    Inserted form with table rows

--- emalda-inc/WebContent/php1/insert_product.php
+++ emalda-inc/WebContent/php1/insert_product.php
@@ -6,46 +6,41 @@
 <body bgcolor="skyblue">
 
 	<form action="insert_product.php" method="post" enctype="multipart/form-data">
-		<table align="center" width="1000" border="2" bgcolor="orange">
+		<table align="center" width="750" border="2" bgcolor="orange">
 		
 			<tr align="center">
 				<td colspan="8"><h2>Insert New Post Here</h2></td>
 			</tr>
 			
 			<tr>
-				<td>Product Title:</td>
+				<td align="right"><b>Product Title:</b></td>
 				<td><input type="text" name="product_title"></td>
 			</tr>
 			
 			<tr>
-				<td>Product Title:</td>
+				<td align="right"><b>Product Price:</b></td>
 				<td><input type="text" name="product_title"></td>
 			</tr>
 			
 			<tr>
-				<td>Product Title:</td>
+				<td align="right"><b>Product Discription:</b></td>
 				<td><input type="text" name="product_title"></td>
 			</tr>
 			
 			<tr>
-				<td>Product Title:</td>
+				<td align="right"><b>Product Image:</b></td>
 				<td><input type="text" name="product_title"></td>
 			</tr>
 			
 			<tr>
-				<td>Product Title:</td>
+				<td align="right"><b>Product Keywords:</b></td>
 				<td><input type="text" name="product_title"></td>
 			</tr>
 			
-			<tr>
-				<td>Product Title:</td>
-				<td><input type="text" name="product_title"></td>
+			<tr align="center">
+				<td colspan="8"><input type="submit" name="insert_post" value="Insert Now"></td>
 			</tr>
 			
-			<tr>
-				<td>Product Title:</td>
-				<td><input type="text" name="product_title"></td>
-			</tr>
 		</table>
 	</form>
 
