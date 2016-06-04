411596d9181dfeca3dbbf3b2c4c5bba714e7952e
Alex Makumbi <alex.makumbi.10@cnu.edu>
2016-06-04 14:25:36 -0400

    created buttons to cart - continue shopping, update cart, and checkout buttons. There functionality was included as well.

--- emalda-inc/WebContent/cart.php
+++ emalda-inc/WebContent/cart.php
@@ -93,17 +93,11 @@ function validateForm() {
 <!--**************************Begin Company Products*****************************-->
 <section class="action-products">
 <div id="products" class="container">
-<div class="container-fluid text-center">
-  <h2 align="center"><a id="products">CART</h2>
-  <div class="products_box">
-  
+<div class="container-fluid">
+
   <form action="" method="post" enctype="multipart/form-data">
   	
-  	<table align="center" width="900" bgcolor="yellow">
-  		
-  		<tr align="center">
-  			<td colspan="5"><h2>Update your cart or checkout</h2></td>
-  		</tr>	
+  	<table align="center" width="730" bgcolor="yellow">	
   		
   		<tr align="center">
   			<th>Remove</th>
@@ -111,13 +105,95 @@ function validateForm() {
   			<th>Quantity</th>
   			<th>Total Price</th>
   		</tr>
-  	
+  		
+  		<?php 
+  		
+  		$total = 0;
+  		
+  		global $con;
+  		
+  		$ip = getIp();
+  		
+  		$sel_price = "SELECT * FROM cart WHERE ip_add='$ip'";
+  		
+  		$run_price = mysqli_query($con, $sel_price);
+  		
+  		while($p_price = mysqli_fetch_array($run_price)){
+  				
+  			$pro_id = $p_price['p_id'];
+  				
+  			$sel_price = "SELECT * FROM products WHERE product_id='$pro_id'";
+  				
+  			$run_pro_price = mysqli_query($con, $sel_price);
+  				
+  			while($pp_price = mysqli_fetch_array($run_pro_price)){
+  		
+  				$product_price = array($pp_price['product_price']);
+  				$product_title = $pp_price['product_title'];
+  				$product_image = $pp_price['product_image'];
+  				$single_price = $pp_price['product_price'];
+  				
+  				$values = array_sum($product_price);
+  		
+  				$total += $values;
+
+  		?>
+  		
+  		<tr align="center">
+  			<td><input type="checkbox" name="remove[]" value="<?php echo $pro_id; ?>"></td>
+  			<td><?php echo $product_title; ?><br>
+  			<img src="admin_area/product_images/<?php echo $product_image;?>" width="60" height="60">
+  			</td>
+  			<td><input type="text" size="3" name="qty"></td>
+  			<td><?php echo "$" . $single_price;?></td>
+  		</tr>
+  		
+  		<?php } } ?>
+  		
+  		<tr align="right">
+  			<td colspan="4"><b>Sub Total</b></td>
+  			<td><?php echo "$" .  $total; ?></td>
+  		</tr>
+  		
+  		<tr align="center">
+  			<td colspan="2"><input type="submit" name="update_cart" value="Update Cart"></td>
+  			<td><input type="submit" name="continue" value="Continue Shopping"></td>
+  			<td><button><a href="checkout.php" style="text-decoration: none; color:black;">Checkout</a></button></td>
+  		</tr>
   	</table>
-   
   </form>	
    
-  
-  </div> 
+   <?php 
+
+   
+	$ip = getIp();
+   
+   	if(isset($_POST['update_cart'])){
+
+		foreach($_POST['remove'] as $remove_id){
+			
+			$delete_product = "DELETE FROM cart WHERE p_id='$remove_id' AND ip_add='$ip'";			
+			$run_delete = mysqli_query($con, $delete_product);
+			
+			if($run_delete){
+				echo "<script>window.open('cart.php', '_self')</script>";
+			}
+
+		}
+   
+   	}
+   	
+   	if(isset($_POST['continue'])){
+
+		echo "<script>window.open('index.php#products', '_self')</script>";
+	}
+   
+   
+   ?>
+   
+   
+   
+   
 </div>
 </div>
 </section> 
