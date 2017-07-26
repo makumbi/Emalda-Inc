e26ceebb9033be15176c4145ac89755760babaf3
Alex Makumbi <alexmakumbi15@gmail.com>
2017-07-26 14:37:16 -0400

    ensured that customer inputs are valided

--- emalda-inc/WebContent/customer/edit_account.php
+++ emalda-inc/WebContent/customer/edit_account.php
@@ -1,8 +1,17 @@
 <?php
   include('includes/db.php');
-  $userEmail = $_SESSION['customer_email'];
-  global $email;
   
+  // Input should be validated
+  $userEmail = test_input($userEmailValidation) = $_SESSION['customer_email'];
+  global $email;
+    
+    // Function used to sanitize and validate use input
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
       // set SQL statement and execute
     $sel_user = "SELECT * FROM customers WHERE customer_email=?";
     
@@ -120,20 +129,75 @@
     }
 ?>
 <?php
+// defining variables and setting them to empty
+$c_name = $c_email = $c_pass = $c_address = $c_city = $c_country = $c_image = $c_image_tmp = $c_contact = "";
+$c_nameErr = $c_emailErr = $c_passErr = $c_addressErr = $c_cityErr = $c_imageErr = $c_image_tmpErr = $c_contactErr = "";
+  
+
 if(isset($_POST['update'])){
     // Retrieves and stores USER IP Address
     $ip = getIp();
-
-    $customer_id = $c_id;
-    $c_name = $_POST['c_name'];
-    $c_email = $_POST['c_email'];
-    $c_pass = $_POST['c_pass'];
-    $c_address = $_POST['c_address'];
-    $c_city = $_POST['c_city'];
+    // user inputs should be sanitized/validated
+    $c_name = test_input($_POST['c_name']);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$c_name)) {
+        $c_nameErr = "Only letters and white space allowed"; 
+        echo "<script>alert('Please try again! $c_nameErr')</script>";
+        exit();
+    }
+    
+    $c_email = test_input($_POST['c_email']);
+    // filter email to validate input
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $c_emailErr = "Invalid email format";
+        echo "<script>alert('Please try again! $c_emailErr')</script>";
+        exit();
+    }
+    
+    $c_pass = test_input($_POST['c_pass']);
+    // check for password length
+    if (iconv_strlen($c_pass) < 8) {
+        $c_passErr = "Password should be longer than 8 characters";
+        echo "<script>alert('Please try again! $c_passErr')</script>";
+        exit();
+    }
+ 
+    $c_address = test_input($_POST['c_address']);
+    // check if email contains valid chars
+    if (!preg_match("/^[a-z0-9-]+$/i",$c_address)) {
+        $c_addressErr = "Invalid address"; 
+        echo "<script>alert('Please try again! $c_addressErr')</script>";
+        exit();
+    }
+    
+    $c_city = test_input($_POST['c_city']);
+    // check if city name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$c_name)) {
+        $c_cityErr = "Only letters and white space allowed";
+        echo "<script>alert('Please try again! $c_cityErr')</script>";
+        exit();
+    }
+    
+    $c_country = test_input($_POST['c_country']);
     $c_image = $_FILES['c_image']['name'];
     $c_image_tmp = $_FILES['c_image']['tmp_name'];
-    $c_contact = $_POST['c_contact'];
+    
+    $c_contact = test_input($_POST['c_contact']);
+    // check if city name only contains letters and whitespace
+    if (!preg_match("/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/",$c_contact)) {
+        $c_contactErr = "Please enter a valid phone number";
+        echo "<script>alert('Please try again! $c_contactErr')</script>";
+        exit();
+    }
 
+    // Function used to sanitize and validate use input
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    
     $hash = password_hash($c_pass, PASSWORD_BCRYPT);
     
     // moves uploaded files into folder called customer_images
