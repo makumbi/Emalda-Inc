eb99f289a6a95b4225de1d7f2c10f9232cc92f23
Alex Makumbi <alexmakumbi15@gmail.com>
2017-08-03 14:07:10 -0400

    image should be properly handled

--- emalda-inc/WebContent/admin_area/edit_pro.php
+++ emalda-inc/WebContent/admin_area/edit_pro.php
@@ -16,7 +16,7 @@ session_start();
         $con = mysqli_connect("localhost", "root", "root", "emaldaDB");
 
         if(mysqli_connect_errno()){
-            echo "The connection was not established: " . mysqli_connect_errno();
+            echo "The database connection was not established";
         }   
 
   if(isset($_GET['edit_pro'])){
@@ -58,103 +58,102 @@ tinymce.init({ selector:'textarea' });
 </script>
 </head>
 <body>
-	<div class="row">
-	  <div class="col-sm-9">
-              <form action="" method="POST" enctype="multipart/form-data">
-                <table align="center" width="740" border="2" bgcolor="yellow">
+    <div class="row">
+      <div class="col-sm-9">
+          <form action="" method="POST" enctype="multipart/form-data">
+            <table align="center" width="740" border="2" bgcolor="yellow">
 
-                    <tr align="center">
-			<td colspan="8"><h2>Edit Product Here</h2></td>
-                    </tr>
+                <tr align="center">
+                    <td colspan="8"><h2>Edit Product Here</h2></td>
+                </tr>
 
-                    <tr>
-			<td align="center"><b>Product Title:</b></td>
-			<td><input type="text" name="product_title" size="60" value="<?php echo $pro_title; ?>"/></td>
-                    </tr>
+                <tr>
+                    <td align="center"><b>Product Title:</b></td>
+                    <td><input type="text" name="product_title" size="60" value="<?php echo $pro_title; ?>"/></td>
+                </tr>
 
-                    <tr>
-                        <td align="center"><b>Product Price:</b></td>
-			<td><input type="text" name="product_price" value="<?php echo $pro_price; ?>"></td>
-                    </tr>
+                <tr>
+                    <td align="center"><b>Product Price:</b></td>
+                    <td><input type="text" name="product_price" value="<?php echo $pro_price; ?>"></td>
+                </tr>
 
-                    <tr>
-                        <td align="center"><b>Product Image:</b></td>
-			<td><input type="file" name="product_image"/><img src="product_images/<?php echo $pro_image; ?>" width="80" height="80" /></td>
-                    </tr>
+                <tr>
+                    <td align="center"><b>Product Image:</b></td>
+                    <td><input type="file" name="product_image"/><img src="product_images/<?php echo $pro_image; ?>" width="80" height="80" /></td>
+                </tr>
 
 
-                    <tr>
-                        <td align="center"><b>Product Description:</b></td>
-			<td><textarea name="product_desc" cols="20" rows="10"><?php echo $pro_desc; ?></textarea></td>
-                    </tr>
+                <tr>
+                    <td align="center"><b>Product Description:</b></td>
+                    <td><textarea name="product_desc" cols="20" rows="10"><?php echo $pro_desc; ?></textarea></td>
+                </tr>
 
-                    <tr>
-			<td align="center"><b>Product Keywords:</b></td>
-			<td><input type="text" name="product_keywords" size="50" value="<?php echo $pro_keywords; ?>"></td>
-                    </tr>
+                <tr>
+                    <td align="center"><b>Product Keywords:</b></td>
+                    <td><input type="text" name="product_keywords" size="50" value="<?php echo $pro_keywords; ?>"></td>
+                </tr>
 
-                    <tr align="center">
-			<td colspan="8"><input type="submit" name="update_product" value="Update Product"></td>
-                    </tr>
+                <tr align="center">
+                    <td colspan="8"><input type="submit" name="update_product" value="Update Product"></td>
+                </tr>
 
-		</table>
-	</form>
-        <br>
-        <br>
-    </div>
+            </table>
+    </form>
+    <br>
+    <br>
+</div>
 </div>
 </body>
 </html>
 <?php
 
-	// Form needs to be validated and sanitized!
-	if(isset($_POST["update_product"])){
-            
-                // Use global keyword to connect to our database
-                global $con;
+// Form needs to be validated and sanitized!
+if(isset($_POST["update_product"])){
 
-                // connect to local DB
-                $con = mysqli_connect("localhost", "root", "root", "emaldaDB");
+    // Use global keyword to connect to our database
+    global $con;
 
-                if(mysqli_connect_errno()){
-                    echo "The connection was not established: " . mysqli_connect_errno();
-                }   
-                $update_id = $pro_id;
-                
-		// POSTting text data from fields
-		$product_title = $_POST["product_title"];
-		$product_price = $_POST["product_price"];
-		$product_desc = $_POST["product_desc"];
-		$product_keywords = $_POST["product_keywords"];
-                
-		// POSTting image from field
-		$product_image = $_FILES["product_image"]["name"];
-		$product_image_tmp = $_FILES["product_image"]["tmp_name"];
-		// Utilizing built in function, $product_image_tmp is store inside $product_image
-		move_uploaded_file($product_image_tmp, "product_images/$product_image");
+    // connect to local DB
+    $con = mysqli_connect("localhost", "root", "root", "emaldaDB");
 
-                // Insert into products table values $product_title','$product_price','$product_desc','$product_image','$product_keywords
-                $update_product = "UPDATE products SET product_title=?, product_price=?, product_desc=?, product_image=?,product_keywords=? WHERE product_id=?";
-                
-                if($stmt = $con -> prepare($update_product)){                   
-                    // bind parameters
-                    $stmt -> bind_param('sisssi', $product_title, $product_price, $product_desc, $product_image, $product_keywords,$update_id);
-                    
-                    // If $insert_prod is a success, alert user that product has been inserted into products table
-                    if($stmt -> execute()){
-			echo "<script>alert('Product has been updated!')</script>";
-			echo "<script>window.open('index.php?view_products', '_self')</script>";
-		}
-                // close statement
-          $stmt ->close();
-            }   
-             
-          // close connection
-         $con -> close(); 
-         echo "<script>alert('Product has NOT been updated!')</script>";
-         echo "<script>window.open('index.php?view_products', '_self')</script>"; 
-        }
-         
-  }        
-     
+    if(mysqli_connect_errno()){
+        echo "The database connection was not established";
+    }   
+    $update_id = $pro_id;
+
+    // validate/sanitize customer input
+    // POSTting text data from fields
+    $product_title = $_POST["product_title"];
+    $product_price = $_POST["product_price"];
+    $product_desc = $_POST["product_desc"];
+    $product_keywords = $_POST["product_keywords"];
+
+    // POSTting image from field
+    $product_image = $_FILES["product_image"]["name"];
+    $product_image_tmp = $_FILES["product_image"]["tmp_name"];
+    // Utilizing built in function, $product_image_tmp is store inside $product_image
+    move_uploaded_file($product_image_tmp, "product_images/$product_image");
+
+    // Insert into products table values $product_title','$product_price','$product_desc','$product_image','$product_keywords
+    $update_product = "UPDATE products SET product_title=?, product_price=?, product_desc=?, product_image=?,product_keywords=? WHERE product_id=?";
+
+    if($stmt = $con -> prepare($update_product)){                   
+        // bind parameters
+        $stmt -> bind_param('sisssi', $product_title, $product_price, $product_desc, $product_image, $product_keywords,$update_id);
+
+        // If $insert_prod is a success, alert user that product has been inserted into products table
+        if($stmt -> execute()){
+            echo "<script>alert('Product has been updated!')</script>";
+            echo "<script>window.open('index.php?view_products', '_self')</script>";
+    }
+    // close statement
+  $stmt ->close();
+    }   
+
+  // close connection
+ $con -> close(); 
+ echo "<script>alert('Product has NOT been updated!')</script>";
+ echo "<script>window.open('index.php?view_products', '_self')</script>"; 
+}
+}           
 ?>
